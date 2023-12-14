f8dad89e22c194a74f5c3b312d765c776f1d685e
Reda Lazrek <reda.23.lazrek@gmail.com>
2023-12-14 16:58:54 +0100

    add Autowired annotation in UserController

--- src/main/java/com/rean/blogrest/controller/UserController.java
+++ src/main/java/com/rean/blogrest/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.rean.blogrest.controller;
 
+import com.rean.blogrest.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/apiblog/users")
 public class UserController {
 
+    @Autowired
+    UserServiceImpl userService;
 }
