0a63879c53b42ab1ac2385643704933e2b2148cd
Reda Lazrek <reda.23.lazrek@gmail.com>
2023-12-25 13:18:09 +0100

    Initialize categories and tags tables in data.sql

---
+++ target/classes/data.sql
@@ -0,0 +1,15 @@
+--INSERTING SOME CATEGORIES
+INSERT INTO categories(name,description) VALUES ('Food',null);
+INSERT INTO categories(name,description) VALUES ('Travel',null);
+INSERT INTO categories(name,description) VALUES ('Health',null);
+INSERT INTO categories(name,description) VALUES ('Fashion and Beauty',null);
+INSERT INTO categories(name,description) VALUES ('Photography',null);
+INSERT INTO categories(name,description) VALUES ('Music',null);
+
+--INSERTING SOME TAGS
+INSERT INTO tags(name) VALUES ('#LifeHacks');
+INSERT INTO tags(name) VALUES ('#TechTrends');
+INSERT INTO tags(name) VALUES ('#TravelInspiration');
+INSERT INTO tags(name) VALUES ('#FoodieFinds');
+INSERT INTO tags(name) VALUES ('#FitnessFiesta');
+INSERT INTO tags(name) VALUES ('#CareerChronicles');
